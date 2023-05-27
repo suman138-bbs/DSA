d7e1ef1beece6afe3fa01122a36a70aa563293ed
Suman Bhandari <91786012+suman138-bbs@users.noreply.github.com>
2023-05-27 10:07:12 +0000

    function array

--- array.cpp
+++ array.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
 using namespace std;
+void f1(int *,int );
 int main(){
- cout  << "Namste world";
+    int a[5];
+    cout << "Enter an array";
+    for(int i=0;i<6;i++){
+        cin >> a[i];
+    }
+    for(int i=0;i<6;i++){
+        cout << a[i] << " ";
+    }
+    f1(a,5);
+  return 0;
+}
+
+void f1(int *p,int s){
+  p[3]=900;
+  cout <<"Inside Function" <<endl;
+  for(int i=0;i<6;i++){
+    
+        cout << p[i] << " ";
+    }
+
 }
