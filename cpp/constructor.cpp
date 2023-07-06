cc4240e5046c70630d81aef52a37544f12402d20
Suman Bhandari <91786012+suman138-bbs@users.noreply.github.com>
2023-07-06 05:05:19 +0000

    constructor

---
+++ cpp/constructor.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+
+// class Cuboid{
+//     int len;
+//     int b;
+//     int h;
+//     public:
+//     Cuboid(int a,int b,int c){
+//         len  = a;
+//         b= b;
+//         h= c;
+//     }
+    
+//   void showData(){
+//     cout << len <<endl<< b <<endl<< h <<endl;
+//   }
+// };
+
+// int main(){
+  
+//   Cuboid c(3,4,5);
+//   c.showData();
+
+// }
+
+
+///////////////////////////////////////////////////////
+// #include<iostream>
+// using namespace std;
+
+// class Customer{
+//     int custId;
+
+//     int email;
+//     int phone;
+//     public:
+//     Customer(int a,int b,int c){
+        
+//     }
+    
+//     Customer(){}
+    
+  
+// };
+
+/////////////////////////////////////////////
+
+// class Complex {
+
+//   int real;
+//   int imgi;
+//   public:
+//   Complex(int a,int b){
+//     real = a;
+//     imgi = b;
+//   }
+
+//   void showData(){
+//       cout << "Real "<<real <<" Imaginary "<<imgi<<endl;
+//   }
+// };
+
+// int main(){
+//   Complex c[5] = {Complex(2,3),Complex(6,3),Complex(7,3),Complex(9,3),Complex(10,3),};
+//   for(int i = 0;i<5;i++){
+//     c[i].showData();
+//   }
+
+// }
+
+
+/////////////////////////////////////////////////////////////////////
+ 
+ class Number {
+
+  int size;
+  int *arr;
+  public:
+  Number(int a,){
+
+  }
+ };
+
+
