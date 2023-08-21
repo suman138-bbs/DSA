fb314e61d89b324edab29f6a6cdceed1820d0b5a
Suman Bhandari <91786012+suman138-bbs@users.noreply.github.com>
2023-08-21 02:50:10 +0000

    overloading of + operator

---
+++ cpp/OperatotOverLoading.cpp
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+
+// class Complex{
+//       int real;
+//       int imgi;
+//       public:
+//       Complex(){
+//         real =0;
+//         imgi = 0;
+
+//       }
+    
+//     Complex operator+(Complex C){
+//            Complex temp;
+//            temp.real = real +C.real;
+//            temp.imgi = imgi +C.imgi;
+//            return temp;
+//     }
+
+//    void data(int val1,int val2){
+//             real = val1;
+//             imgi = val2;
+//    }
+
+//    void showData(){
+//     cout << "Real" <<real<<" "<<"Imiginary" <<imgi;
+//    }
+
+
+
+// };
+
+// int main(){
+//     Complex c,c1,c2;
+//     c1.data(2,3);
+//     c2.data(3,2);
+//     c = c1+c2;
+//     c.showData();
+// }
+
+
+// Overloading of insertion and extration operator
+
+
+class Time{
+      int hr;
+      int min;
+      int sec;
+      public:
+      Time(){
+         hr = 0;
+        min = 0;
+        sec = 0;
+
+      }
+   
+
+   void data(int hour,int min,int sec){
+           hour = hour;
+           min=min;
+           sec = sec;
+   }
+   void showData(){
+  
+   }
+
+
+
+};
+
+int main(){
+    Time t1,t2;
+    t1.data(3,40,41);
+    t2.data(4,40,41);
+    cout<<t1;
+}
--- cpp/constructor.cpp
+++ cpp/constructor.cpp
@@ -61,68 +61,68 @@ using namespace std;
  * Implement deep copy to avoid memory location 
 */
 
-// class Number{
-//     int size;
-//     int *arr;
-//     public:
-//     Number(){}
-//     Number(int s){
-//         size =s;
-//        arr= new int[s];
-//        cout <<"Array created Successfully"<<endl;
-//     }
-//    void addToArray(int inc){
-//         for(int i=0;i<size;i++){
-//             arr[i]=i+inc;
-//         }
-//     }
-//     void show(){
-//         for(int i=0;i<size;i++){
-//             cout << arr[i] <<" ";
-//         }
-//         cout <<endl;
-//     }
-
-//     Number(Number &n){
-//         size = n.size;
-//         arr =   new int [size];
-        
-//         for(int i =0;i<size;i++){
-            
-//            arr[i]=n.arr[i];
-           
-//         }
-
-//     }
-//     ~Number(){
-//         delete []arr;
-//     }
-
-    
-// };
-
-// int main(){
-//     Number n1(5);
-//     n1.addToArray(10);
-//     Number n2(n1);
-//     n2.show();
-//     n2.addToArray(50);
-//     n1.show();
-//     n2.show();
-    
-    
-// }
-
-
-class Date{
-    int d,m,y;
-  public:
-    Date():d(26),m(8),y(2000)
-    {
-        cout <<d<<'-'<<m<<'-'<<y;
+class Number{
+    int size;
+    int *arr;
+    public:
+    Number(){}
+    Number(int s){
+        size =s;
+       arr= new int[s];
+       cout <<"Array created Successfully"<<endl;
     }
+   void addToArray(int inc){
+        for(int i=0;i<size;i++){
+            arr[i]=i+inc;
+        }
+    }
+    void show(){
+        for(int i=0;i<size;i++){
+            cout << arr[i] <<" ";
+        }
+        cout <<endl;
+    }
+
+    Number(Number &n){
+        size = n.size;
+        arr =   new int [size];
+        
+        for(int i =0;i<size;i++){
+            
+           arr[i]=n.arr[i];
+           
+        }
+
+    }
+    ~Number(){
+        delete []arr;
+    }
+
     
 };
+
+int main(){
+    Number n1(5);
+    n1.addToArray(10);
+    Number n2(n1);
+    n2.show();
+    n2.addToArray(50);
+    n1.show();
+    n2.show();
+    
+    
+}
+
+
+// class Date{
+//     int d,m,y;
+//   public:
+//     Date():d(26),m(8),y(2000)
+//     {
+//         cout <<d<<'-'<<m<<'-'<<y;
+//     }
+    
+// };
 
 int main(){
     Date d1;
