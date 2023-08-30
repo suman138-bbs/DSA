c0f0ddf93023e6c189cc6fd339aa3c6341d05d47
Suman Bhandari <91786012+suman138-bbs@users.noreply.github.com>
2023-08-30 06:19:08 +0000

    queue using array1

---
+++ DSA/Queue/queueUsingLinkList.cpp
@@ -0,0 +1,27 @@
+#include<stdint.h>
+#include<iostream>
+using namespace std;
+struct node
+{
+    int item;
+    node *next;
+
+};
+
+class Queue
+{
+private:
+    node *ptr;
+    no
+public:
+    Queue();
+    ~Queue();
+};
+
+Queue::Queue()
+{
+}
+
+Queue::~Queue()
+{
+}
